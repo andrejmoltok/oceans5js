3342ca6e5025a97019537ad3764714baaa1c80ca
andrejmoltok <csokan.andras87@protonmail.ch>
2024-03-27 05:35:08 +0100

    O5T-17 Add Speakeasy for MFA/TOTP authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Page() {
-  return <>Oceans5 - Battleship clone game</>;
+  return (
+    <>
+      Oceans5 - Battleship clone game
+      <div>
+        <a href="./signup">SignUp</a>
+      </div>
+    </>
+  );
 }
